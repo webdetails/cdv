6be7495a42fe8762c44f6a59621e36aaee390f9f
Pedro Pinheiro <pdpinheiro@gmail.com>
2012-06-05 15:53:25 +0100

    Add timing library
    Note that later.js is thoroughly patched to work
    in Rhino.

---
+++ resources/js/lib/timers.js
@@ -0,0 +1,43 @@
+var setTimeout,
+    clearTimeout,
+    setInterval,
+    clearInterval;
+
+(function (global) {
+    var timer = new java.util.Timer();
+    var counter = 1; 
+    var ids = {};
+
+    setTimeout = function (fn,delay) {
+        var id = counter++;
+        ids[id] = new JavaAdapter(java.util.TimerTask,{run: function(){
+          fn.apply(global, Array.prototype.slice.call(arguments,2)); 
+        }});
+        timer.schedule(ids[id],delay);
+        return id;
+    }
+
+    clearTimeout = function (id) {
+        print ("cancelling " + id);
+        if(ids[id]) {
+          print ("found id");
+          ids[id].cancel();
+        }
+        timer.purge();
+        delete ids[id];
+    }
+
+    setInterval = function (fn,delay) {
+        var id = counter++; 
+        ids[id] = new JavaAdapter(java.util.TimerTask,{run: function(){
+          fn.apply(global, Array.prototype.slice.call(arguments,2));
+        }});
+        timer.schedule(ids[id],delay,delay);
+        return id;
+    }
+
+    clearInterval = clearTimeout;
+
+})(this);
+
+
