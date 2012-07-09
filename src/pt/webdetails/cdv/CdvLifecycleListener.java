1dae71f03bf8a6651f2f8231f983bcd8e30be3b5
Pedro Vale <pedro.vale@webdetails.pt>
2012-07-09 19:26:21 +0100

    ensure folders are created if they do not exist

--- src/pt/webdetails/cdv/CdvLifecycleListener.java
+++ src/pt/webdetails/cdv/CdvLifecycleListener.java
@@ -4,15 +4,27 @@
 
 package pt.webdetails.cdv;
 
+import java.io.IOException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.pentaho.platform.api.engine.IPentahoSession;
 import org.pentaho.platform.api.engine.IPluginLifecycleListener;
+import org.pentaho.platform.api.engine.IUserDetailsRoleListService;
 import org.pentaho.platform.api.engine.PluginLifecycleException;
+import org.pentaho.platform.api.repository.ISolutionRepository;
+import org.pentaho.platform.api.repository.ISolutionRepositoryService;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.engine.core.system.UserSession;
+import org.pentaho.platform.engine.security.SecurityHelper;
+import org.springframework.security.Authentication;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.providers.anonymous.AnonymousAuthenticationToken;
 import pt.webdetails.cdv.notifications.NotificationEngine;
 import pt.webdetails.cdv.operations.PushWarningsHandler;
 import pt.webdetails.cdv.scripts.GlobalScope;
 import pt.webdetails.cpf.RestRequestHandler.HttpMethod;
 import pt.webdetails.cpf.persistence.PersistenceEngine;
+import pt.webdetails.cpf.repository.RepositoryAccess;
 
 /**
  * This class inits Cdv plugin within the bi-platform
@@ -23,6 +35,19 @@ public class CdvLifecycleListener implements IPluginLifecycleListener {
 
     static Log logger = LogFactory.getLog(CdvLifecycleListener.class);
 
+    
+   private static IPentahoSession getAdminSession() {
+        IUserDetailsRoleListService userDetailsRoleListService = PentahoSystem.getUserDetailsRoleListService();
+        UserSession session = new UserSession("admin", null, false, null);
+        GrantedAuthority[] auths = userDetailsRoleListService.getUserRoleListService().getAllAuthorities();
+        Authentication auth = new AnonymousAuthenticationToken("admin", SecurityHelper.SESSION_PRINCIPAL, auths);
+        session.setAttribute(SecurityHelper.SESSION_PRINCIPAL, auth);
+        session.doStartupActions(null);
+        return session;
+    }
+     
+    
+    
     public void init() throws PluginLifecycleException {
         reInit();
     }
@@ -38,8 +63,40 @@ public class CdvLifecycleListener implements IPluginLifecycleListener {
         scope.executeScript("system/cdv/js/bootstrap.js");
     }
 
-    public void loaded() throws PluginLifecycleException {
+  @Override
+  public void loaded() throws PluginLifecycleException {
+    //Check if folder cdb and cdb/saiku and cdb/queries exist
+    IPentahoSession adminSession = getAdminSession();
+    ISolutionRepository solutionRepository = PentahoSystem.get(ISolutionRepository.class, adminSession);
+    ISolutionRepositoryService repService = PentahoSystem.get(ISolutionRepositoryService.class, adminSession);
+    if (!solutionRepository.resourceExists("cdv")) {
+      try {
+        repService.createFolder(adminSession, "", "", "cdv", "CDV");                      
+      } catch (IOException ioe) {
+        logger.error("Error while creating folder cdv for cdv plugin. CDV will not work", ioe);
+      }
     }
+    
+    if (!solutionRepository.resourceExists("cdv/tests")) {
+      try {
+        repService.createFolder(adminSession, "", "cdv", "tests", "tests");                
+      } catch (IOException ioe) {
+        logger.error("Error while creating folder cdv/tests for cdv plugin. CDV will not work", ioe);
+      }      
+    }
+
+    if (!solutionRepository.resourceExists("cdv/notifications.xml")) {
+      try {
+
+        RepositoryAccess.getRepository().copySolutionFile("system/cdv/notifications.xml", "cdv/notifications.xml");
+      } catch (IOException ioe) {
+        logger.error("Error while creating default notifications.xml for CDV plugin. CDV will use the one at system/cdv", ioe);
+      }      
+    }
+    
+    
+    
+  }
 
     public void unLoaded() throws PluginLifecycleException {
     }
