3bf78f8c2bb608a4f86d8d97f80f06a21c135fc6
Pedro Alves <pedro.alves@webdetails.pt>
2012-08-14 12:39:24 +0100

    - Added the ability to get sample tests from CDV - Build.xml now publishes samples too

--- src/pt/webdetails/cdv/CdvContentGenerator.java
+++ src/pt/webdetails/cdv/CdvContentGenerator.java
@@ -21,6 +21,7 @@ import org.apache.commons.lang.StringUtils;
 import org.json.JSONException;
 import org.pentaho.platform.api.engine.IParameterProvider;
 
+import org.pentaho.platform.api.engine.ISolutionFile;
 import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
 import pt.webdetails.cdv.operations.PushWarningsHandler;
 import pt.webdetails.cpf.InterPluginCall;
@@ -83,68 +84,113 @@ public class CdvContentGenerator extends RestContentGenerator {
         callCDE("notificationSettings.wcdf", out);
     }
 
-
-    
     private JSONObject createTest(String origin, String newPath) throws JSONException {
-        
+
         JSONObject result = new JSONObject();
-        if (!newPath.endsWith(".cdv"))
-          newPath = newPath + ".cdv";
+        if (!newPath.endsWith(".cdv")) {
+            newPath = newPath + ".cdv";
+        }
         String newFileName = "cdv/tests/" + newPath;
-            
+
         RepositoryAccess repAccess = RepositoryAccess.getRepository();
         try {
-          if (repAccess.resourceExists(newFileName)) {
-            logger.error("New File already exists, aborting creatio of new test");
-            result.put("success", "false");
-            return result;
-          }
-              
-          String originalTest = repAccess.getResourceAsString(origin);
-          
-                             
-          originalTest = Pattern.compile("path:\\s*['\"].*['\"]\\s*,").matcher(originalTest).replaceFirst("path: '" + newFileName + "',");
-          originalTest = Pattern.compile("name:\\s*['\"].*['\"]\\s*,").matcher(originalTest).replaceFirst("name: '" + newFileName.substring(newFileName.lastIndexOf("/")+1).replaceAll(Matcher.quoteReplacement(".cdv"), "")  + "',");
-          originalTest = Pattern.compile("createdBy:\\s*['\"].*['\"]\\s*,").matcher(originalTest).replaceFirst("createdBy: '" + PentahoSessionHolder.getSession().getName()  + "',");
-          repAccess.publishFile(newFileName, originalTest, false);
-          result.put("success", "true");
-          result.put("path", newFileName);
+            if (repAccess.resourceExists(newFileName)) {
+                logger.error("New File already exists, aborting creatio of new test");
+                result.put("success", "false");
+                return result;
+            }
+
+            String originalTest = repAccess.getResourceAsString(origin);
+
+
+            originalTest = Pattern.compile("path:\\s*['\"].*['\"]\\s*,").matcher(originalTest).replaceFirst("path: '" + newFileName + "',");
+            originalTest = Pattern.compile("name:\\s*['\"].*['\"]\\s*,").matcher(originalTest).replaceFirst("name: '" + newFileName.substring(newFileName.lastIndexOf("/") + 1).replaceAll(Matcher.quoteReplacement(".cdv"), "") + "',");
+            originalTest = Pattern.compile("createdBy:\\s*['\"].*['\"]\\s*,").matcher(originalTest).replaceFirst("createdBy: '" + PentahoSessionHolder.getSession().getName() + "',");
+            repAccess.publishFile(newFileName, originalTest, false);
+            result.put("success", "true");
+            result.put("path", newFileName);
         } catch (IOException ioe) {
-          logger.error("Error while creating test file", ioe);
-          result.put("success", "false");              
+            logger.error("Error while creating test file", ioe);
+            result.put("success", "false");
         }
-      
+
         return result;
     }
-    
-    
+
+    private JSONObject copyCDVTestsFromPluginSamples() throws JSONException {
+
+        JSONObject result = new JSONObject();
+        RepositoryAccess repAccess = RepositoryAccess.getRepository();
+        try {
+
+            // 1. Get files from path - do I need to create the dir?
+
+            final String origin = "plugin-samples/cdv/";
+            final String destination = "cdv/tests/";
+
+            if (repAccess.canWrite(destination)) {
+
+                List<String> extensions = new ArrayList<String>();
+                extensions.add("cdv");
+                ISolutionFile[] tests = repAccess.listSolutionFiles(origin, RepositoryAccess.FileAccess.READ, false, extensions);
+                for (ISolutionFile test : tests) {
+                    repAccess.copySolutionFile(test.getSolutionPath() + "/"+ test.getFileName(), destination + test.getFileName());
+
+                }
+
+                // Call Refresh tests - InterPluginCall to the same plugin is actually the simplest way
+                InterPluginCall pluginCall = new InterPluginCall(InterPluginCall.CDV, "refreshTests");
+                pluginCall.call();
+
+
+            } else {
+                result.put("success", "false");
+                return result;
+            }
+
+            //repAccess.publishFile(newFileName, originalTest, false);
+            result.put("success", "true");
+
+        } catch (IOException ioe) {
+            logger.error("Error while creating test file", ioe);
+            result.put("success", "false");
+        }
+
+        return result;
+        
+    }
+
     @Exposed(accessLevel = AccessLevel.PUBLIC, outputType = MimeType.JSON)
     public void newTest(OutputStream out) throws IOException, JSONException {
-      String newName = getRequestParameters().getStringParameter("newName", null);
-      JSONObject result = createTest("system/cdv/validationTemplate.cdv", newName);      
-       writeOut(out, result.toString(2));      
+        String newName = getRequestParameters().getStringParameter("newName", null);
+        JSONObject result = createTest("system/cdv/validationTemplate.cdv", newName);
+        writeOut(out, result.toString(2));
     }
-    
-    
-    
+
     @Exposed(accessLevel = AccessLevel.PUBLIC, outputType = MimeType.JSON)
     public void duplicateTest(OutputStream out) throws IOException, JSONException {
         String path = getRequestParameters().getStringParameter("path", null);
         String newName = getRequestParameters().getStringParameter("newName", null);
         JSONObject result;
-        
+
         //Need to validate this a bit more - ensure that path is in cdv/tests at least
-        if (path != null) {          
+        if (path != null) {
             result = createTest(path, newName);
         } else {
-          result = new JSONObject();
-          result.put("success", "false");
+            result = new JSONObject();
+            result.put("success", "false");
         }
         writeOut(out, result.toString(2));
     }
-    
-    
-    
+
+    @Exposed(accessLevel = AccessLevel.PUBLIC, outputType = MimeType.JSON)
+    public void copyCDVTests(OutputStream out) throws IOException, JSONException {
+        JSONObject result;
+
+        result = copyCDVTestsFromPluginSamples();
+        writeOut(out, result.toString(2));
+    }
+
     //TODO: TEMP!
     @Exposed(accessLevel = AccessLevel.ADMIN, outputType = MimeType.PLAIN_TEXT)
     public void deleteTable(OutputStream out) throws IOException {
@@ -193,7 +239,7 @@ public class CdvContentGenerator extends RestContentGenerator {
     private void callCDE(String file, OutputStream out) throws UnsupportedEncodingException, IOException {
 
         ServletRequest wrapper = getRequest();
-        String root = wrapper.getScheme() + "://"+ wrapper.getServerName() + ":" + wrapper.getServerPort();
+        String root = wrapper.getScheme() + "://" + wrapper.getServerName() + ":" + wrapper.getServerPort();
 
         Map<String, Object> params = new HashMap<String, Object>();
         params.put("solution", "system");
