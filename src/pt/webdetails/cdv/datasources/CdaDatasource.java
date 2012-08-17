1746ba56ec2e272005c5c5b7987f39a5b58f1294
tgf <tiago.n.ferreira@gmail.com>
2012-08-17 20:15:39 +0100

    unused imports

--- src/pt/webdetails/cdv/datasources/CdaDatasource.java
+++ src/pt/webdetails/cdv/datasources/CdaDatasource.java
@@ -3,22 +3,12 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 package pt.webdetails.cdv.datasources;
 
-import java.io.ByteArrayOutputStream;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.pentaho.platform.api.engine.IContentGenerator;
-import org.pentaho.platform.api.engine.IOutputHandler;
-import org.pentaho.platform.api.engine.IParameterProvider;
-import org.pentaho.platform.api.engine.IPentahoSession;
-import org.pentaho.platform.api.engine.IPluginManager;
-import org.pentaho.platform.engine.core.output.SimpleOutputHandler;
-import org.pentaho.platform.engine.core.solution.SimpleParameterProvider;
-import org.pentaho.platform.engine.core.system.PentahoSessionHolder;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
 import pt.webdetails.cpf.InterPluginCall;
 
 /**
@@ -36,6 +26,7 @@ public class CdaDatasource implements Datasource {
     private String getQueryData() {
 
         InterPluginCall pluginCall = new InterPluginCall(InterPluginCall.CDA, "doQuery", requestMap);
+        //TODO:response
         return pluginCall.callInPluginClassLoader();
     }
     
